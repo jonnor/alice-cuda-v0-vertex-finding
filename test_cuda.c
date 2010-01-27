70570c2ea5ae0c8ec2250c3e89ee3bf35759f0c9
Jon Nordby <jononor@gmail.com>
2010-01-27 21:25:28 +0100

    Implemented Evaluate. Untested.
    Also fixed a bug due to uninitialized pointer.

--- test_cuda.c
+++ test_cuda.c
@@ -1,12 +1,12 @@
 
-
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
+#include <float.h>
 
 // http://aliceinfo.cern.ch/static/aliroot-new/html/roothtml/src/AliVParticle.h.html#18
 const double kB2C=-0.299792458e-3;
-
+const double kAlmost0=FLT_MIN;
 
 // http://aliceinfo.cern.ch/static/aliroot-new/html/roothtml/src/AliExternalTrackParam.h.html#178
 // We could also just use a simple array instead of the struct
@@ -44,11 +44,36 @@ void GetHelixParameters(struct trackparam *tp, double hlx[6], double b) {
 } 
 
 
+// http://aliceinfo.cern.ch/static/aliroot-new/html/roothtml/src/AliExternalTrackParam.cxx.html#RJz9EE
+static void Evaluate(const double *h, double t,
+                     double r[3],  //radius vector
+                     double g[3],  //first defivatives
+                     double gg[3]) //second derivatives
+{
+
+  double phase=h[4]*t+h[2];
+  double sn=sin(phase), cs=cos(phase);
+
+  r[0] = h[5];
+  r[1] = h[0];
+  if (fabs(h[4])>kAlmost0) {
+     r[0] += (sn - h[6])/h[4];
+     r[1] -= (cs - h[7])/h[4];  
+  }
+  r[2] = h[1] + h[3]*t;
+
+  g[0] = cs; g[1]=sn; g[2]=h[3];
+  
+  gg[0]=-h[4]*sn; gg[1]=h[4]*cs; gg[2]=0.;
+}
+
+
 int main()
 {
 
     double b = -5.00668;
     struct trackparam *tp;
+    tp = (struct trackparam*)malloc(sizeof(struct trackparam));
     tp->fP[0] = -0.00429969;
     tp->fP[1] = -4.56162;
     tp->fP[2] = 2.38928e-09;
@@ -61,6 +86,12 @@ int main()
     GetHelixParameters(tp, hp, b);
     printf("GetHelixParameters\n");
     for (int i; i<6; i++) printf("%f\n", hp[i]);
+
+
+    // TODO: find real inputdata and test
+    double t = 3; 
+    double rv[3], d[3], dd[3];
+    Evaluate(hp, t, rv, d, dd);
 
     return 1;
 }

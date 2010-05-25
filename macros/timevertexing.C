03a57f586da8b88876bf0dadcf2e83f5972d6ef6
Lars Bratrud <Lars.Bratrud@student.hive.no>
2010-05-26 00:06:24 +0200

    Updated macro for timing v0 vertexing.

--- macros/timevertexing.C
+++ macros/timevertexing.C
@@ -1,15 +1,18 @@
 #include "TFile.h"
 #include "TTree.h"
-#include <stdio.h>
 #include "TStopwatch.h"
-#define RUNS 20
+#include "TXMLEngine.h"
+#include "TXMLSetup.h"
+#include "TCanvas.h"
+#include "TStorage.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 AliESDEvent* load_event() {
 	// Load .root
 	// http://www-numi.fnal.gov/offline_software/srt_public_context/WebDocs/Companion/root_crib/ttree.html
-	TFile *esdfile = new TFile("AliESDs.root","READ");  //read root-file
+	TFile *esdfile = new TFile("AliESDs.root","READ");
 	TTree *esdTree = (TTree *) esdfile->Get("esdTree"); //read tree
-
 	AliESDEvent *event = new AliESDEvent();
 	event->ReadFromTree(esdTree);
 	esdTree->GetEvent();
@@ -20,34 +23,51 @@ time_vertexing(AliESDEvent* event, Float_t* array, Int_t RUNS) {
 	AliV0vertexer v0vertexer;
 	TStopwatch *tstopw = new TStopwatch();
 	Int_t i;
-
-  for(i=0;i<RUNS;i++){
-  tstopw->Start();
-  Int_t N_v0 = v0vertexer.Tracks2V0vertices(event);
-  tstopw->Stop();
-  array[i] = tstopw->RealTime();
- }
+	AliESDEvent *eventcopy = new AliESDEvent();
+	for(i=0;i<RUNS;i++){
+	 // AliESDEvent *eventcopy = new AliESDEvent();
+	  (*eventcopy) = (*event);
+	  tstopw->Start();
+	  Int_t N_v0 = v0vertexer.Tracks2V0vertices(eventcopy);
+	  tstopw->Stop();
+	  array[i] = tstopw->RealTime();
+	  //delete eventcopy;
+	  eventcopy->Reset();
+	}
 
 }
 
-TH1F* draw_histrogram(Float_t array[]) {
-// Draw histogram from data
+TH1F* draw_histogram(Float_t array[], Int_t N, Int_t nbinsx) {
+	
+	Float_t M = 0;
+	Float_t xlow = 0;
+	Float_t xup = 0;
+	Float_t mean = 0;
+	for(i=0;i<N;i++){
+		M = M + array[i];
+	}	
+	mean = M / N;
+	xlow = mean * 0.96;
+	xup = mean * 1.04;
+
+	// Draw histogram from data
 	TCanvas *c1;
 	// Create canvas with grid
 	c1 = new TCanvas("c1","Blopp",600,400);
 	c1->SetGrid();
 	// create histogram
-	hist = new TH1F("times","V0Vertexer time distribution",100,1.4,1.6);
+	hist = new TH1F("times","V0Vertexer time distribution", nbinsx, xlow, xup);
 	hist->SetFillColor(16);
-	hist->SetMarkerStyle(21);
+	hist->SetMarkerStyle(20);
 	hist->SetMarkerSize(0.7);
 	hist->Draw("hmm");
 
-for(int i; i<RUNS; i++){ 
+for(int i=0; i<N; i++){ 
 	hist->Fill(array[i]); }
 
   c1->Modified();
   c1->Update();
+  c1->Print("histogram.pdf","pdfLandscape");
 	return hist;
 }
 
@@ -57,5 +77,29 @@ void PrintArray(Float_t array[], Int_t N)
 // Print results
 for(i=0;i<N;i++)
  printf("%f",array[i]);
+}
+void save_data(Float_t array[], Int_t N, Char_t filename[]){
+	//TFile *file = new TFile("Data.xml","RECREATE");  //create file
+	//hist->Write();
+	//delete file;
+	FILE *file;
+	file = fopen(filename,"a+");
+	for(i=0;i<N;i++)	
+		fprintf(file, "%f\n",array[i]);
+	fclose(file);
+	//return 0;
+}
+
+void load_data(Float_t array[], Char_t filename[], Int_t N){
+        FILE *ifile;
+        ifile = fopen(filename,"r");
+        Int_t i = 0;
+        //array = new Float_t[N];
+        for(i = 0; i<N; i++){		
+		Int_t ret = fscanf(ifile, "%f\n", &array[i]);
+		if(ret == EOF) break;	
+	}
+        fclose(ifile);
+        //return 0;
 }
 
